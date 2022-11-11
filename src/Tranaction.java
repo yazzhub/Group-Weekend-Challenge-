f9020d6184d43ed359cc9fea741cda095455fadf
Yasmin <yasmin@Yasmins-MacBook-Air-2.local>
2022-11-11 10:59:58 -0600

    Initial Commit

---
+++ src/Tranaction.java
@@ -0,0 +1,63 @@
+public class Tranaction {
+
+   private String accountHolder;
+
+   private int transactionNumber;
+
+   private double purchasePrice;
+
+   private String purchaseName;
+
+
+    public Tranaction(String accountHolder, int transactionNumber, double purchasePrice, String purchaseName) {
+        this.accountHolder = accountHolder;
+        this.transactionNumber = transactionNumber;
+        this.purchasePrice = purchasePrice;
+        this.purchaseName = purchaseName;
+
+
+
+    }
+
+    public String getAccountHolder() {
+        return accountHolder;
+    }
+
+    public void setAccountHolder(String accountHolder) {
+        this.accountHolder = accountHolder;
+    }
+
+    public int getTransactionNumber() {
+        return transactionNumber;
+    }
+
+    public void setTransactionNumber(int transactionNumber) {
+        this.transactionNumber = transactionNumber;
+    }
+
+    public double getPurchasePrice() {
+        return purchasePrice;
+    }
+
+    public void setPurchasePrice(double purchasePrice) {
+        this.purchasePrice = purchasePrice;
+    }
+
+    public String getPurchaseName() {
+        return purchaseName;
+    }
+
+    public void setPurchaseName(String purchaseName) {
+        this.purchaseName = purchaseName;
+    }
+
+    @Override
+    public String toString() {
+        return "Tranaction{" +
+                "accountHolder='" + accountHolder + '\'' +
+                ", transactionNumber=" + transactionNumber +
+                ", purchasePrice=" + purchasePrice +
+                ", purchaseName='" + purchaseName + '\'' +
+                '}';
+    }
+}
